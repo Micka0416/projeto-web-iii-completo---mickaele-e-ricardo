00396d78669d1b3a5e6d143cd82af067045e069a
Micka0416 <136914161+Micka0416@users.noreply.github.com>
2023-12-09 20:39:10 -0300

    Create Atendente.js

---
+++ Atendente.js
@@ -0,0 +1,9 @@
+class Atendente{
+    constructor(id, cpf, nome, email){
+        this.id=id;
+        this.cpf=cpf;
+        this.nome=nome;
+        this.email=email;
+    }
+}
+module.exports = Atendente;
