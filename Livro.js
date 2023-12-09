c5e2f52e293a78113e1883a3c0dbaf957a99c208
Micka0416 <136914161+Micka0416@users.noreply.github.com>
2023-12-09 20:40:17 -0300

    Create Livro.js

---
+++ Livro.js
@@ -0,0 +1,9 @@
+class Livro{
+    constructor(id,titulo, autor, isbn){
+        this.id=id;
+        this.titulo=titulo;
+        this.autor=autor;
+        this.isbn=isbn;
+    }
+}
+module.exports = Livro;
